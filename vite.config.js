86e6302ffa9cddd2f15e48cf41d5aa8bea05e8ee
xperiencegroup <xperiencerealestate.cc@gmail.com>
2026-09-18 18:30:21 -0600

    Set base path for deployment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/truecienegaindustrialpark/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
